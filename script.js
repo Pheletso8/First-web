784fe0f17a4ce8cc30f56b3fc18d8883199515f3
Pheletso8 <pheletso.marumoloe8@gmail.com>
2025-05-07 16:04:26 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggleIcon = document.querySelector('.toggle-icon');
+
+toggleIcon.addEventListener('click', () => {
+    toggleIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+});
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.header');
+    header.classList.toggle('scrolled', window.scrollY > 50);
+});
